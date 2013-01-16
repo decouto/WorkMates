cf90d73145049f5f53c810fad40c17391091ed29
David Danko <dcdanko@mit.edu>
2013-01-16 00:19:35 -0500

    HomePage

--- django_WM/HomePage/views.py
+++ django_WM/HomePage/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("This is the home page. You found it.")
+	return HttpResponse("This is the home page for WorkMates. You found it.")
